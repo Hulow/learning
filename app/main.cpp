2ffc791da54ec7939511dcac280b1aff4ceeea81
viq hulow <viq.hlw@gmail.com>
2026-03-05 11:48:30 +0100

    chore: clean executable file

--- app/main.cpp
+++ app/main.cpp
@@ -6,13 +6,13 @@ int main() {
     Console console;
     MaskColorUseCase usecase(console);
     const MaskColorUseCaseDto dto = MaskColorUseCaseDto::from(250, 100, 50, std::bitset<8>(0b00110010));
-    auto [prevRed, prevBits, maskBits, newBits, newRed] = usecase.execute(dto);
+    auto [currentColor, currentColorBits, maskingColor, newColorBits, newColor] = usecase.execute(dto);
 
-    std::cout << std::to_string(prevRed) << std::endl;
-    std::cout << prevBits.to_string() << std::endl;
-    std::cout <<  maskBits.to_string() << std::endl;
-    std::cout <<  newBits.to_string() << std::endl;
-    std::cout <<  std::to_string(newRed) << std::endl;
+    std::cout << std::to_string(currentColor) << std::endl;
+    std::cout << currentColorBits.to_string() << std::endl;
+    std::cout <<  maskingColor.to_string() << std::endl;
+    std::cout <<  newColorBits.to_string() << std::endl;
+    std::cout <<  std::to_string(newColor) << std::endl;
 
     return 0;
 };
