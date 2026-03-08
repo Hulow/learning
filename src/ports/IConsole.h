5f706fe09bdc9a803ba8b1e524af6df95e20516d
viq hulow <viq.hlw@gmail.com>
2026-03-08 17:23:31 +0100

    feat: add note

--- src/ports/IConsole.h
+++ src/ports/IConsole.h
@@ -3,9 +3,15 @@
 
 class IConsole {
     public:
-        virtual ~IConsole() = default;
+        virtual ~IConsole() = default; 
         virtual std::string prompt(const std::string message) = 0;
         virtual void write(const std::string message) = 0;
         virtual void writeError(const std::string message) = 0;
         virtual void inform(const std::string message) = 0;
 };
+
+/*
+    ~IConsole() -> destructor
+    virtual ~IConsole() -> polymorphic destruction
+    = default -> use compiler-generated destructor
+*/
